a2117100c43b9e4806acba76fef4bb4fe3a35943
hsantosdias@gmail.com <hsantosdias@gmail.com>
2025-02-04 17:54:56 -0300

    Adiciona suporte para LED verde e refatora inicialização dos todos os LEDs no BitDogLab-Semaforo.c

--- BitDogLab-Semaforo.c
+++ BitDogLab-Semaforo.c
@@ -4,6 +4,7 @@
 
 #define LED_PIN_RED 13  // Pino do LED vermelho
 #define LED_PIN_BLUE 12 // Pino do LED azul
+#define LED_PIN_GREEN 11 // Pino do LED verde
 #define TIMER_INTERVAL_MS 1000  // Intervalo do temporizador (1 segundo)
 #define SERIAL_PRINT_INTERVAL 10  // Número de ciclos para imprimir na serial
 
@@ -16,6 +17,7 @@ bool repeating_timer_callback(struct repeating_timer *t) {
     leds_on = !leds_on;
     gpio_put(LED_PIN_RED, leds_on);
     gpio_put(LED_PIN_BLUE, leds_on);
+    gpio_put(LED_PIN_GREEN, leds_on);
     
     // Imprime mensagem na serial
     printf("1 segundo passou\n");
@@ -26,27 +28,30 @@ bool repeating_timer_callback(struct repeating_timer *t) {
 int main() {
     stdio_init_all(); // Inicializa a comunicação serial
 
-    // Inicializa e configura os LEDs RED e BLUE
+    // Inicializa e configura os LEDs RED e BLUE e GREEN
     gpio_init(LED_PIN_RED);
     gpio_init(LED_PIN_BLUE);
+    gpio_init(LED_PIN_GREEN);
     gpio_set_dir(LED_PIN_RED, GPIO_OUT);
     gpio_set_dir(LED_PIN_BLUE, GPIO_OUT);
+    gpio_set_dir(LED_PIN_GREEN, GPIO_OUT);
     gpio_put(LED_PIN_RED, false); // Garante que o LED começa desligado
     gpio_put(LED_PIN_BLUE, false); // Garante que o LED começa desligado
+    gpio_put(LED_PIN_GREEN, false); // Garante que o LED começa desligado
 
     // Configura o temporizador para chamar a função callback a cada 1s
     struct repeating_timer timer;
     add_repeating_timer_ms(TIMER_INTERVAL_MS, repeating_timer_callback, NULL, &timer);
 
-    // Loop infinito que mantém o programa rodando
+    // Loop principal que mantém o programa rodando
     while (true) {
-        sleep_ms(1000); // Agora verifica a cada 1s
-        counter++;  // Incrementa o contador
+        sleep_ms(1000);
+        counter++;
 
         // Apenas imprime a cada SERIAL_PRINT_INTERVAL segundos
         if (counter >= SERIAL_PRINT_INTERVAL) {
             printf("Rotina de repetição\n");
-            counter = 0; // Reseta o contador
+            counter = 0;
         }
     }
 
